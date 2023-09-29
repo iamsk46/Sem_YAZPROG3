8f7eb24f0344ab9caacab6842e1b77fd53aaa829
Iamsk46 <lomalstekloy@gmail.com>
2023-09-29 20:22:49 +0300

    Zadacha 4

---
+++ Z4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    int negativeNumber = -number;
+    while (negativeNumber <= number)
+    {
+        Console.Write(negativeNumber + ", ");
+        negativeNumber++;        
+    }
+}
+else
+{
+    System.Console.WriteLine("Введите положительное число!");
+}
