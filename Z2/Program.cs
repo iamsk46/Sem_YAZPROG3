c35ec6803daf776d1f0062984cc7140dbd977600
Iamsk46 <lomalstekloy@gmail.com>
2023-09-29 19:08:58 +0300

    Zadacha 2

---
+++ Z2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number * number == square)
+{
+Console.WriteLine("True");
+Console.Read();
+}
+else {
+Console.WriteLine("False");
+Console.Read();
+}
