1d42c5cbce95d4c9897f8ca55ed8d45c0d7b0353
Iamsk46 <lomalstekloy@gmail.com>
2023-10-09 17:41:48 +0300

    Sem

---
+++ Z17/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// System.Console.WriteLine("ВВедите координату x: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("ВВедите координату y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if ( x > y && y > 0)
+// {
+//     System.Console.WriteLine("Наша точка находится в первой четверти");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     System.Console.WriteLine("Наша точка находится во второй четверти");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     System.Console.WriteLine("Наша точка находится в третьей четверти");
+// }
+// else
+// {
+//     System.Console.WriteLine("Наша точка находится в четвертой четверти");
+// }
+
+void Nazvanie(int x, int y)
+{
+
+if ( x > y && y > 0)
+{
+    System.Console.WriteLine("Наша точка находится в первой четверти");
+}
+
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("Наша точка находится во второй четверти");
+}
+
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("Наша точка находится в третьей четверти");
+}
+
+else
+{
+    System.Console.WriteLine("Наша точка находится в четвертой четверти");
+}
+
+}
+
+System.Console.WriteLine("ВВедите координату x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("ВВедите координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Nazvanie(x, y);
---
+++ Z18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+string[] quaters = {"x > 0, y > 0", // индекс 0 хранится ответ для первой четверти
+"x < 0, y > 0", // индекс 1 хранится ответ для второй четверти
+"x < 0, y < 0", // индекс 2 хранится ответ для третьей четверти
+"x > 0, y < 0" // индекс 3 хранится ответ для четвертой четверти
+};
+
+System.Console.Write("Введите номер четверти: ");
+int quater = Convert.ToInt32(Console.ReadLine());
+if (quater >= 1 && quater <= 4)
+{
+System.Console.WriteLine(quaters[quater - 1]);
+}
+else
+{
+System.Console.WriteLine("Такой четверти не существует");
+}
+
+
+
+// double num = 8;
+// double res = Math.Sqrt(num);
+// System.Console.WriteLine(Math.Pow(num, 2)); // Math.Pow() - возведение в степень
+// System.Console.WriteLine(Math.Sqrt(num)); // Извлечение квадратного корня
+// System.Console.WriteLine(Math.Round(res, 3)); // Округление дробного числа
